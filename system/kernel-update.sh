6ef862a60000df5696efd59e2358422ebe25823d
chowes <chowes@uwaterloo.ca>
2017-10-30 18:46:58 -0400

    kernel upgrade script - changed relative directories to absolute

--- system/kernel-update.sh
+++ system/kernel-update.sh
@@ -4,24 +4,26 @@ echo "This needs to be run as root"
 
 echo "Downloading Linux kernel 4.13.10"
 
-wget --directory-prefix=~/ https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.13.10.tar.xz
-tar -xf ~/linux-4.13.10.tar.xz
-cp .config ~/linux-4.13.10/
+wget --directory-prefix=/home/chowes/ https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.13.10.tar.xz
+tar -xf /home/chowes/linux-4.13.10.tar.xz
+cp /home/chowes/data-center-bbr/system/.config /home/chowes/linux-4.13.10/
 
 echo "Compiling Linux kernel..."
 
-make -j 12 -C ~/linux-4.13.10/
+make -j 12 -C /home/chowes/linux-4.13.10/
 
 echo "Installing modules..."
 
-make -j 12 -C ~/linux-4.13.10/ modules_install
+make -j 12 -C /home/chowes/linux-4.13.10/ modules_install
 
 echo "Installing new kernel..."
 
-make -j 12 -C ~/linux-4.13.10/ install
+make -j 12 -C /home/chowes/linux-4.13.10/ install
 
 update-initramfs -c -k 4.13.10
 update grub
 
 echo "Rebooting into new kernel..."
+
+reboot
 
