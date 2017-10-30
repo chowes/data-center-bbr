d2ed548651e7d091002f8a0458b39d9b4f6f2084
chowes <chowes@uwaterloo.ca>
2017-10-30 14:45:44 -0400

    Prototype of config script

--- system/kernel-update.sh
+++ system/kernel-update.sh
@@ -1,3 +1,27 @@
 #!/bin/bash
-echo "hello world!"
+
+echo "This needs to be run as root"
+
+echo "Downloading Linux kernel 4.13.10"
+
+wget --directory-prefix=~/ https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.13.10.tar.xz
+tar -xf ~/linux-4.13.10.tar.xz
+cp .config ~/linux-4.13.10/
+
+echo "Compiling Linux kernel..."
+
+make -j 12 -C ~/linux-4.13.10/
+
+echo "Installing modules..."
+
+make -j 12 -C ~/linux-4.13.10/ modules_install
+
+echo "Installing new kernel..."
+
+make -j 12 -C ~/linux-4.13.10/ install
+
+update-initramfs -c -k 4.13.10
+update grub
+
+echo "Rebooting into new kernel..."
 
