52d7d21897a1b9d72061024bc7339e6de5a9e91e
chowes <chowes@uwaterloo.ca>
2017-11-11 04:20:43 -0500

    Fixed an error in the gender file, fixed a bug preventing grub from updating in the kernel-update script

--- system/kernel-update.sh
+++ system/kernel-update.sh
@@ -5,18 +5,9 @@ if ! [ $(id -u) = 0 ]; then
 	exit 1
 fi
 
-echo "Downloading Linux kernel 4.13.10"
-wget --directory-prefix=/home/chowes/ https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.13.10.tar.xz
-
-echo "Extracting kernel..."
-tar -xf /home/chowes/linux-4.13.10.tar.xz
-
-echo "Copying config file into compile directory"
-cp /home/chowes/data-center-bbr/system/.config /home/chowes/linux-4.13.10/
-
 
 echo "Compiling..."
-make -j 12 -C /home/chowes/linux-4.13.10/
+# make -j 12 -C /home/chowes/linux-4.13.10/
 
 echo "Installing modules..."
 make -j 12 -C /home/chowes/linux-4.13.10/ modules_install
@@ -24,7 +15,7 @@ make -j 12 -C /home/chowes/linux-4.13.10/ modules_install
 echo "Installing new kernel..."
 make -j 12 -C /home/chowes/linux-4.13.10/ install
 update-initramfs -c -k 4.13.10
-update grub
+update-grub
 
 echo "Rebooting into new kernel..."
 reboot
