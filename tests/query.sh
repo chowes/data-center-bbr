e10457868d744a61289472f42b14b4e11493bfbc
chowes <chowes@uwaterloo.ca>
2017-11-21 05:06:45 -0500

    results for query test - bbr

--- tests/query.sh
+++ tests/query.sh
@@ -5,25 +5,29 @@ cong_ctl=$1
 aggregator="blue00"
 mb_size=1048576
 
-for (( i = 1; i <= 1; i++ )); do
+filename="/home/chowes/data-center-bbr/tests/results/query/query_"$cong_ctl".csv" 
+cp "/home/chowes/data-center-bbr/tests/results/query/query_template.csv" $filename
+
+for (( i = 1; i <= 20; i++ )); do
 	for ((j = 0; j < 10; j++)); do
 		
-		# start the aggregator
-		pdsh -w $aggregator /home/chowes/data-center-bbr/aggregator query $i $mb_size "query_test_"$cong_ctl".csv"
-
 		# build the worker list
 		worker_list=""
 		# read the first i worker nodes from file
 		input="./nodes"
-		for (( j = 1; j <= $i; j++ )); do
+		for (( k = 1; k <= $i; k++ )); do
 			read -r line
 			worker_list+="$line"
 			worker_list+=","
-			echo $worker_list
 		done < "$input"
-
-		# start the workers
+		
+		echo $j
+		
+		# start the aggregator and workers
+		pdsh -w $aggregator /home/chowes/data-center-bbr/aggregator query $i $mb_size $filename &
+		sleep 1
 		pdsh -w $worker_list /home/chowes/data-center-bbr/worker $aggregator
+		sleep 1
 
 	done
 done
