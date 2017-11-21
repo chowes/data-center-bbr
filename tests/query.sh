d8c55f0adbc2220a4a88478342b270ce5d0ba7e0
chowes <chowes@uwaterloo.ca>
2017-11-21 03:29:52 -0500

    removed debug output on test script

--- tests/query.sh
+++ tests/query.sh
@@ -9,8 +9,7 @@ for (( i = 1; i <= 20; i++ )); do
 	for ((j = 0; j < 10; j++)); do
 		
 		# start the aggregator
-		echo pdsh -w $aggregator /home/chowes/data-center-bbr/aggregator query $i $mb_size "query_test_"$cong_ctl".csv"
-
+		pdsh -w $aggregator /home/chowes/data-center-bbr/aggregator query $i $mb_size "query_test_"$cong_ctl".csv"
 
 		# build the worker list
 		worker_list=""
@@ -23,9 +22,8 @@ for (( i = 1; i <= 20; i++ )); do
 			echo $worker_list
 		done < "$input"
 
-		
 		# start the workers
-		echo pdsh -w $worker_list /home/chowes/data-center-bbr/worker $aggregator
+		pdsh -w $worker_list /home/chowes/data-center-bbr/worker $aggregator
 
 	done
 done
