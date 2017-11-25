a34db76d76a841a83857b9c114f410e53dcc5a53
chowes <chowes@uwaterloo.ca>
2017-11-25 18:54:09 -0500

    40 node tests

--- tests/query.sh
+++ tests/query.sh
@@ -8,26 +8,24 @@ mb_size=1048576
 filename="/home/chowes/data-center-bbr/tests/results/query/query_"$cong_ctl".csv" 
 cp "/home/chowes/data-center-bbr/tests/results/query/query_template.csv" $filename
 
-for (( i = 1; i <= 20; i++ )); do
-	for ((j = 0; j < 1; j++)); do
-		
-		# build the worker list
-		worker_list=""
-		# read the first i worker nodes from file
-		input="./nodes"
-		for (( k = 1; k <= $i; k++ )); do
-			read -r line
-			worker_list+="$line"
-			worker_list+=","
-		done < "$input"
-		
-		echo $j
+for (( i = 21; i <= 40; i++ )); do
+	for ((j = 0; j < 100; j++)); do
 		
 		# start the aggregator and workers
 		pdsh -w $aggregator /home/chowes/data-center-bbr/aggregator query $i $mb_size $filename &
 		sleep 3
-		pdsh -w $worker_list /home/chowes/data-center-bbr/worker $aggregator
-		sleep 3
 
+		# start the workers
+		
+		# read the first i worker nodes from file
+		input="./nodes"
+		for (( k = 1; k <= $i; k++ )); do
+			read -r line
+			worker="$line"
+			echo "starting worker: $worker"
+			pdsh -w $worker /home/chowes/data-center-bbr/worker $aggregator &
+		done < "$input"
+
+		sleep 3
 	done
 done
