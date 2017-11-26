2bd7dbc788e346adbe03a4a83f96ce304333f1f9
chowes <chowes@uwaterloo.ca>
2017-11-25 20:33:36 -0500

    prelim throughput tests

--- tests/thru.sh
+++ tests/thru.sh
@@ -3,29 +3,33 @@
 cong_ctl=$1
 
 aggregator="blue00"
-test_length=30
+test_length=10
 
 filename="/home/chowes/data-center-bbr/tests/results/throughput/thru_"$cong_ctl".csv" 
 cp "/home/chowes/data-center-bbr/tests/results/throughput/thru_template.csv" $filename
 
 for (( i = 1; i <= 20; i++ )); do
-                
-    # start the aggregator and workers
-    pdsh -w $aggregator /home/chowes/data-center-bbr/aggregator throughput $i $test_length .5 $filename &
-    aggregator_pid=$!
-    sleep 1
+    for (( j = 0; j < 100; j++ )); do            
 
-    # start the workers
+	echo "flows: $i - iter: $j"
+
+        # start the aggregator and workers
+    	pdsh -w $aggregator /home/chowes/data-center-bbr/aggregator throughput $i $test_length $testlength $filename &
+    	aggregator_pid=$!
+   	sleep 3
+
+    	# start the workers
     
-    # read the first i worker nodes from file
-    input="./nodes"
-    for (( k = 1; k <= $i; k++ )); do
+    	# read the first i worker nodes from file
+    	input="./nodes"
+    	for (( k = 1; k <= $i; k++ )); do
             read -r line
             worker="$line"
             echo "starting worker: $worker"
             pdsh -w $worker /home/chowes/data-center-bbr/worker $aggregator &
-    done < "$input"
+    	done < "$input"
 
-    wait
-    sleep 1
+    	wait
+    	sleep 3
+    done
 done
